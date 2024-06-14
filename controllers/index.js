e79a54a63f2373e6cf29af3d5f4e1aeb168b52d4
mikedaleo <mikedaleo@outlook.com>
2024-06-13 20:41:56 -0400

    edited some handlebars and connection file

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./api/userRoutes');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const storyRoutes = require('./api/storyRoutes');
-const completedStoryRoutes = require('./api/completedStoryRoutes');
+
 router.use('/', homeRoutes);
-router.use('/api/user', userRoutes);
-router.use('/api/story', storyRoutes);
-router.use('/api/completedStory', completedStoryRoutes);
+router.use('/api', apiRoutes);
+
 
 module.exports = router;
 
